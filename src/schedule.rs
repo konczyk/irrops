40293ec08311e60a95cc7bd46792d6e9573e0923
Daniel Kończyk <daniel@konczyk.net>
2026-01-06 12:47:56 +0100

    Add aircraft-flight assignment

--- src/schedule.rs
+++ src/schedule.rs
@@ -12,4 +12,27 @@ impl Schedule {
         Schedule { aircraft, flights }
     }
 
+    fn assign(&mut self)  {
+        let mut busy = self.aircraft.iter()
+            .map(|(id, ac)| {
+                (id.clone(), ac.disruptions.iter().map(|d| (d.from.to_minutes(), d.to.to_minutes())).collect())
+            }).collect::<HashMap<String, Vec<(u16, u16)>>>();
+
+        self.flights.sort_by(|a, b| a.departure_time.cmp(&b.departure_time));
+        self.flights.iter_mut().for_each(|flight| {
+            let ac = busy.iter().find(|(_, blocks)| {
+                blocks.iter().all(|(from, to)| {
+                    flight.departure_time > *to || flight.arrival_time < *from
+                })
+            }).map(|(id, _)| id.clone());
+
+            if let Some(id) = ac {
+                flight.aircraft_id = Some(id.clone());
+                if let Some(intervals) = busy.get_mut(&id) {
+                   intervals.push((flight.departure_time, flight.arrival_time))
+
+                }
+            }
+        });
+    }
 }
